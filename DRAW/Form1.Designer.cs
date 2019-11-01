5dafcd1715c09bf17d499c92c7e66c246bf0786d
student <student@kstu.edu.ru>
2019-11-01 12:15:38 +0300

    my comment

---
+++ DRAW/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace DRAW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.ColorBox = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.программаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.новыйФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.crug = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.новыйToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ColorBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.PictureBox1_Click);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(976, 470);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 44);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Палитра";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // ColorBox
+            // 
+            this.ColorBox.BackColor = System.Drawing.Color.Black;
+            this.ColorBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.ColorBox.Location = new System.Drawing.Point(828, 470);
+            this.ColorBox.Name = "ColorBox";
+            this.ColorBox.Size = new System.Drawing.Size(60, 57);
+            this.ColorBox.TabIndex = 2;
+            this.ColorBox.TabStop = false;
+            this.ColorBox.Click += new System.EventHandler(this.ColorBox_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(925, 222);
+            this.trackBar1.Maximum = 15;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(192, 56);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.программаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1231, 28);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // программаToolStripMenuItem
+            // 
+            this.программаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьToolStripMenuItem,
+            this.выходToolStripMenuItem,
+            this.открытьToolStripMenuItem,
+            this.новыйФайлToolStripMenuItem,
+            this.новыйToolStripMenuItem});
+            this.программаToolStripMenuItem.Name = "программаToolStripMenuItem";
+            this.программаToolStripMenuItem.Size = new System.Drawing.Size(103, 24);
+            this.программаToolStripMenuItem.Text = "программа";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.сохранитьToolStripMenuItem.Text = "сохранить";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.СохранитьToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.выходToolStripMenuItem.Text = "выход";
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.открытьToolStripMenuItem.Text = "открыть";
+            this.открытьToolStripMenuItem.Click += new System.EventHandler(this.ОткрытьToolStripMenuItem_Click);
+                      
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.radioButton1.Location = new System.Drawing.Point(828, 67);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(97, 21);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Карандаш";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton1_CheckedChanged);
+            // 
+            // crug
+            // 
+            this.crug.AutoSize = true;
+            this.crug.Location = new System.Drawing.Point(832, 118);
+            this.crug.Name = "crug";
+            this.crug.Size = new System.Drawing.Size(58, 21);
+            this.crug.TabIndex = 6;
+            this.crug.Text = "Круг";
+            this.crug.UseVisualStyleBackColor = true;
+            this.crug.CheckedChanged += new System.EventHandler(this.Crug_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(828, 159);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(130, 21);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Прямоугольник";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.RadioButton3_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(976, 597);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(173, 44);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Отчистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // новыйToolStripMenuItem
+            // 
+            this.новыйToolStripMenuItem.Name = "новыйToolStripMenuItem";
+            this.новыйToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.новыйToolStripMenuItem.Text = "Новый";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1231, 700);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.crug);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.ColorBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Paint";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ColorBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox ColorBox;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem программаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton crug;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripMenuItem новыйФайлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem новыйToolStripMenuItem;
+    }
+}
+
---
+++ DRAW/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DRAW
+{
+    public partial class Form1 : Form
+    {
+        Bitmap pic;
+        Bitmap pic1;
+        string mode;
+        int x1, y1;
+        int xcl1, ycl1;
+        public Form1()
+        {
+             mode = "Линия";
+            InitializeComponent();
+            
+            pic = new Bitmap(800, 600);
+            pic1 = new Bitmap(800, 600);
+            x1 = y1 = 0;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            ColorBox.BackColor = colorDialog1.Color;
+
+
+        }
+
+        private void ColorBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void СохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            if(saveFileDialog1.FileName!="")
+            pic.Save(saveFileDialog1.FileName);
+            
+               
+        }
+
+        private void ОткрытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            if(openFileDialog1.FileName!="")
+            {
+                pic = (Bitmap)Image.FromFile(openFileDialog1.FileName);
+                pictureBox1.Image = pic;
+            }
+        }
+
+        private void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            mode = "Линия";
+        }
+
+        private void Crug_CheckedChanged(object sender, EventArgs e)
+        {
+            mode = "Круг";
+        }
+
+        private void RadioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            mode = "Прямоугольник";
+        }
+
+        private void PictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Pen p;
+            p = new Pen(ColorBox.BackColor, trackBar1.Value);
+            Graphics g;
+            g = Graphics.FromImage(pic);
+            if (mode == "Прямоугольник")
+            {
+
+               
+                g.DrawRectangle(p, xcl1, ycl1,e.X - xcl1, e.Y - ycl1);
+            }
+
+            if (mode == "Круг")
+            {
+                
+                g.DrawEllipse(p, xcl1, ycl1, e.X - xcl1, e.Y - ycl1);
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Graphics g;
+            g = Graphics.FromImage(pic);
+            g.Clear(Color.White);
+            pictureBox1.Image = pic;
+        }
+
+       
+
+        private void PictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+           xcl1= e.X;
+            ycl1= e.Y;
+        }
+
+        private void PictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Pen p;
+            p = new Pen(ColorBox.BackColor, trackBar1.Value);
+            
+            p.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+            p.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            //p.CustomStartCap.BaseCap = System.Drawing.Drawing2D.LineCap.Round;
+            Graphics g;
+            g = Graphics.FromImage(pic);
+            Graphics g1;
+            g1 = Graphics.FromImage(pic1);
+
+
+            if (e.Button == MouseButtons.Left)
+            {
+                if (mode == "Линия")
+                {
+                    g.DrawLine(p, x1, y1, e.X, e.Y);
+                }
+
+                if (mode == "Прямоугольник")
+                {
+                    g1.Clear(Color.White);
+                    int x, y;
+                    x = xcl1;
+                    y = ycl1;
+                    if (x > e.X) x = e.X;
+                    if (y > e.Y) y = e.Y;
+                  
+                    g1.DrawRectangle(p, x, y, Math.Abs(e.X-xcl1), Math.Abs(e.Y-ycl1));
+                }
+
+                if (mode == "Круг")
+                {
+                    g1.Clear(Color.White);
+                   
+                    g1.DrawEllipse(p, xcl1, ycl1, e.X - xcl1, e.Y - ycl1);
+                }
+                g1.DrawImage(pic, 0, 0);
+
+                pictureBox1.Image = pic1;
+               
+            }
+            x1 = e.X;
+            y1 = e.Y;
+        }
+    }
+}
